b134ebc392f961e1af912a3deeb14c1ade00f646
k0102575 <k0102575@gmail.com>
2020-05-21 18:32:15 +0900

    UsePreventLeave

--- hooks/src/components/UsePreventLeave.js
+++ hooks/src/components/UsePreventLeave.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
+const usePreventLeaveFunc = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = '';
+  };
+  const enablePrevent = () => window.addEventListener('beforeunload', listener);
+  const disabledPrevent = () =>
+    window.removeEventListener('beforeunload', listener);
+
+  return {
+    enablePrevent,
+    disabledPrevent,
+  };
+};
+
 function UsePreventLeave() {
+  const { enablePrevent, disabledPrevent } = usePreventLeaveFunc();
+
   return (
     <div>
       <h3>UsePreventLeave</h3>
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disabledPrevent}>unProtect</button>
       <hr />
     </div>
   );
